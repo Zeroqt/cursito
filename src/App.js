e12c491f43b0045f4edd16e37ae7f78845f24b31
Michel Herrera <mhprotoss@gmail.com>
2023-03-03 16:29:00 -0500

    commit 1.0

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          esto es un ejemplo react.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
